ffbb133c79a7c846381bb4903d735e1533c99798
Fidiniaina Andrianaivo <fidy@andrianaivo.org>
2019-02-19 16:24:55 +0100

    (TESTS): fixing goss testing

--- tests/goss/goss.sh
+++ tests/goss/goss.sh
@@ -17,10 +17,16 @@ export ANSIBLE_INVENTORY="${scriptFullDir}/inventory"
 export ANSIBLE_ROLES_PATH="$(dirname ${scriptParentDir})"
 
 
-# run test playbook
+# run goss tests
+DOCKER_IMAGE="docker4prime/ciborg"
 GOSSFILE_PATH=${scriptFullDir}/goss.yml
-echo "[>] running goss test defined in ${GOSSFILE_PATH}"
-goss --gossfile ${GOSSFILE_PATH} validate -f documentation
+if docker version >/dev/null 2>&1;then
+  echo "[>] running goss test defined in ${GOSSFILE_PATH} (via docker image ${DOCKER_IMAGE})"
+  docker run --rm -it -v ${GOSSFILE_PATH}:/tmp/goss.yml ${DOCKER_IMAGE} goss --gossfile /tmp/goss.yml validate -f documentation
+else
+  echo "[>] running goss test defined in ${GOSSFILE_PATH}"
+  goss --gossfile ${GOSSFILE_PATH} validate -f documentation
+fi
 
 
 # eof
